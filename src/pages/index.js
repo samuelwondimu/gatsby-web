977f6955c19dd0cceec71650544ba790374ddf4f
samuelwondimu <wsamuelwondimu@gmail.com>
2022-02-28 18:41:36 +0300

    sample pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
-import React from "react"
+import React from "react";
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <h2>I'm Samuel A Frontend developer living in Addis Ababa</h2>
+        </div>
+    );
+}
+
+export default IndexPage;
